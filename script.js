9c8368da24b3b924e4bedf49ab803ffef818423b
codepilot2010 <93812156+codepilot2010@users.noreply.github.com>
2023-05-24 20:25:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+function one()
+{
+    window.location.href = "1.html";
+}
+function two()
+{
+    window.location.href = "2.html";
+}
+function three()
+{
+    window.location.href = "3.html";
+}
+function four()
+{
+    window.location.href = "4.html";
+}
+function five()
+{
+    window.location.href = "5.html";
+}
+function six()
+{
+    window.location.href = "6.html";
+}
+function seven()
+{
+    window.location.href = "7.html";
+}
+function eight()
+{
+    window.location.href = "8.html";
+}
+
+function home()
+{
+    window.location.href = "index.html";
+}
